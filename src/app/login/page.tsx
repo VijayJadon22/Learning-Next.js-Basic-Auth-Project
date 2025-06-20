e6a0fdcb88de1a48e5873d0f298e589e447d5f5f
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-20 15:19:56 +0530

    Implemnted handleLogin function which sends an axios post request to the api and handles user login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,35 +3,39 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import toast from "react-hot-toast";
 
 export default function LoginPage() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const router = useRouter();
 
-  const handleLogin = async () => {};
+  const [loading, setLoading] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post("/api/users/login", formData);
+      console.log("login sucess, response: ", response.data);
+      router.push("/");
+    } catch (error:any) {
+      console.log("Error logingin user: ", error);
+      toast.error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-40 py-4 border">
       <form
         onSubmit={handleLogin}
         className="border w-full flex flex-col items-center space-y-4 p-4"
       >
-        <h1>Signup</h1>
-        {/* <input
-          onChange={(e) =>
-            setFormData((prevState) => ({
-              ...prevState,
-              username: e.target.value,
-            }))
-          }
-          className="py-1 px-4 border bg-white text-gray-500 outline-none rounded-lg"
-          id="username"
-          type="text"
-          name="username"
-          value={formData.username}
-          placeholder="Username"
-        /> */}
+        <h1>Login</h1>
+
         <input
           onChange={(e) =>
             setFormData((prevState) => ({
@@ -61,7 +65,7 @@ export default function LoginPage() {
           placeholder="Password"
         />
         <button className="px-8 py-1 bg-white text-gray-700 rounded-lg cursor-pointer">
-          Signup
+          {loading ? "Loging in" : "Login"}
         </button>
         <Link href={"/signup"}>Visit signup Page</Link>
       </form>
