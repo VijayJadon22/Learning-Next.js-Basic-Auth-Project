2e949b45c6f0b08fd0b80fa43d9867a329d2597d
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-06-20 15:30:15 +0530

    Added a check in connectToDB fucntion so that multiple instances dont get created

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,9 +1,13 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectToDB = async () => {
+    if (isConnected) return;
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI!);
-        console.log(`Connected To DB: ${conn.connection.host}`)
+        isConnected = true;
+        console.log(`Connected To DB: ${conn.connection.host}`);
     } catch (error) {
         console.log("Error connecting to DB: ", error);
         process.exit(1);
